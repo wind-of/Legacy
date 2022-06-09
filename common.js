d6fa948b4f220df014fe3d26e09dcbe100b22f9c
wind-of <adequation@mail.ru>
2022-06-09 09:34:45 +0300

    Listed operators

--- common.js
+++ common.js
@@ -9,9 +9,19 @@ const NULL = "null"
 const TRUE = "true"
 const FALSE = "false"
 
+const BitwiseAssignmentOperators = new Set(["&=", "|=", "<<=", ">>=", ">>>=", "^="])
+const LogicalAssignmentOperators = new Set(["&&=", "||=", "??="])
+const AssignmentOperators = new Set(["=", "+=", "-=", "/=", "%=", "*=", "**="])
+
+const assignmentOperators = new Set([...BitwiseAssignmentOperators, ...LogicalAssignmentOperators, ...AssignmentOperators])
+const arithmeticOperators = new Set(["+", "-", "/", "*", "%"])
+const bitwiseOperators = new Set(["~", "|", "~", "&", ">>", ">>>", "<<", "^"])
+const logicalOperators = new Set([">", "<", "<=", ">=", "==", "!=", "===", "!==", "&&", "||", "!", "?", "??"])
+const specialPunctuators = new Set(["(", ")", "[", "]", "{", "}", ":", ";", ".", "...", ",", "=", "=>", "++", "--", "**"])
+
 const digits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-const punctuators = new Set(["++", "+", "--", "-", "~", "/", "**", "*", "|", "&", "%", "(", ")", "{", "}", "[", "]", ">", "<", ">>>", ">>", "<<", "^", "=", "==", "===", "=>", "?", "!", "<=", ">=", ":", ";", "?", ".", "...", ","])
-const keywords = new Set(["break", "case", "catch", "continue", "debugger", "default", "delete", "do", "else", "finally", "for", "function", "if", "in", "instanceof", "new", "return", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "class", "const", "enum", "export", "extends", "import", "super", "implements", "interface", "let", "package", "private", "protected", "public", "static", "yield", "null", "true", "false"])
+const punctuators = new Set([...arithmeticOperators, ...bitwiseOperators, ...bitwiseOperators, ...specialPunctuators, ...assignmentOperators])
+const keywords = new Set(["break", "case", "catch", "continue", "debugger", "default", "delete", "do", "else", "finally", "for", "function", "if", "in", "instanceof", "new", "return", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "class", "const", "enum", "export", "extends", "import", "super", "implements", "interface", "let", "package", "private", "protected", "public", "static", "yield", NULL, TRUE, FALSE, UNDEFINED])
 
 const TokenTypes = ["Punctuator", "Numeric", "Identifier", "Keyword", "String", "Literal"].reduce((a, c) => (a[c] = c, a), {})
 const NodeTypes = ["Identifier", "Literal"].reduce((a, c) => (a[c] = c, a), {})
@@ -19,6 +29,8 @@ const StatementTypes = ["ExpressionStatement"].reduce((a, c) => (a[c] = c, a), {
 const VariableTypes = new Set(["const", "let", "var"])
 
 const UnaryOperators = new Set(["++", "--", "+", "-", "!", "~", "typeof", "delete", "void"])
+const BinaryOperators = new Set(["+", "-", "instanceof", "in", ...assignmentOperators, ...logicalOperators])
+const TernaryOperators = new Set(["?"])
 
 const isLiteral = (token) => [UNDEFINED, NULL, TRUE, FALSE].includes(token.value) || [TokenTypes.String, TokenTypes.Numeric].includes(token.type)
 const handleLiteral = ({ value }) => {
@@ -45,7 +57,10 @@ module.exports = {
   NodeTypes,
   StatementTypes,
   VariableTypes,
+
   UnaryOperators,
+  BinaryOperators,
+  TernaryOperators,
 
   Token,
   Error_,
